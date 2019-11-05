cbe75c21caf47526215dd00f489e71defb22bae4
Henry Phan <h.phan06@gmail.com>
2019-11-05 01:35:48 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,11 +1,21 @@
-def unsafe?(speed)
+require 'pry'
 
+def unsafe?(speed)
+  
+  if speed > 60 || speed < 40
+    true
+  else
+    false
+  end
 end
 
 
 
 def not_safe?(speed)
-	
+  #binding.pry
+  (40...60).include?speed ? true : false
+  speed >= 60 ? true : false || speed <= 40 ? true : false
+  
 end
 	
 
