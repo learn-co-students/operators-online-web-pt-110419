da66ae8acf73630b0213c185fc5fb0c3e196634c
Mintra Phophaijit <bluembc@gmail.com>
2019-11-09 20:55:47 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,17 +1,33 @@
-speed == 79
 def unsafe?(speed)
-    return !!true
-  elsif speed < 35
-    return !!true
-  elsif speed 40 <= 60
-  return !!true
-elsif speed < 40
-return !!true
-else speed 40 <= 60
-  return !!false
+  speed < 40 || speed > 60 ? true : false
 end
 
+def not_safe?(speed)
+  speed < 40 || speed > 60 ? true : false
+end
 
+def sum_machine(first, second)
+  if first < second
+    low = first
+    high = second
+  else
+    low = second
+    high = first
+  end
+
+  sum = 0
+  (low..high).each do |num|
+    sum += num
+  end
+  sum
+end
+
+def squares(collection)
+  collection.collect do |num|
+    num ||= 0
+    num*num
+  end
+end
 
 
 
